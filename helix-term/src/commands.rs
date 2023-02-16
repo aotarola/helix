32f7308a72225d3267e12ce3c4a0cbccd6d23825
Pascal Kuthe <pascal.kuthe@semimod.de>
2023-02-16 22:15:06 +0100

    fix panic when deleting overlapping ranges
    Some deletion operations (especially those that use indentation)
    can generate overlapping deletion ranges when using multiple cursors.
    To fix that problem a new `Transaction::delete` and
    `Transaction:delete_by_selection` function were added. These functions
    merge overlapping deletion ranges instead of generating an invalid
    transaction. This merging of changes is only possible for deletions
    and not for other changes and therefore require its own function.
    
    The function has been used in all commands that currently delete
    text by using `Transaction::change_by_selection`.

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -2317,9 +2317,8 @@ fn delete_selection_impl(cx: &mut Context, op: Operation) {
     };
 
     // then delete
-    let transaction = Transaction::change_by_selection(doc.text(), selection, |range| {
-        (range.from(), range.to(), None)
-    });
+    let transaction =
+        Transaction::delete_by_selection(doc.text(), selection, |range| (range.from(), range.to()));
     doc.apply(&transaction, view.id);
 
     match op {
@@ -2335,9 +2334,8 @@ fn delete_selection_impl(cx: &mut Context, op: Operation) {
 
 #[inline]
 fn delete_selection_insert_mode(doc: &mut Document, view: &mut View, selection: &Selection) {
-    let transaction = Transaction::change_by_selection(doc.text(), selection, |range| {
-        (range.from(), range.to(), None)
-    });
+    let transaction =
+        Transaction::delete_by_selection(doc.text(), selection, |range| (range.from(), range.to()));
     doc.apply(&transaction, view.id);
 }
 
@@ -3467,10 +3465,10 @@ pub mod insert {
         let auto_pairs = doc.auto_pairs(cx.editor);
 
         let transaction =
-            Transaction::change_by_selection(doc.text(), doc.selection(view.id), |range| {
+            Transaction::delete_by_selection(doc.text(), doc.selection(view.id), |range| {
                 let pos = range.cursor(text);
                 if pos == 0 {
-                    return (pos, pos, None);
+                    return (pos, pos);
                 }
                 let line_start_pos = text.line_to_char(range.cursor_line(text));
                 // consider to delete by indent level if all characters before `pos` are indent units.
@@ -3478,11 +3476,7 @@ pub mod insert {
                 if !fragment.is_empty() && fragment.chars().all(|ch| ch == ' ' || ch == '\t') {
                     if text.get_char(pos.saturating_sub(1)) == Some('\t') {
                         // fast path, delete one char
-                        (
-                            graphemes::nth_prev_grapheme_boundary(text, pos, 1),
-                            pos,
-                            None,
-                        )
+                        (graphemes::nth_prev_grapheme_boundary(text, pos, 1), pos)
                     } else {
                         let width: usize = fragment
                             .chars()
@@ -3509,7 +3503,7 @@ pub mod insert {
                                 _ => break,
                             }
                         }
-                        (start, pos, None) // delete!
+                        (start, pos) // delete!
                     }
                 } else {
                     match (
@@ -3527,17 +3521,12 @@ pub mod insert {
                             (
                                 graphemes::nth_prev_grapheme_boundary(text, pos, count),
                                 graphemes::nth_next_grapheme_boundary(text, pos, count),
-                                None,
                             )
                         }
                         _ =>
                         // delete 1 char
                         {
-                            (
-                                graphemes::nth_prev_grapheme_boundary(text, pos, count),
-                                pos,
-                                None,
-                            )
+                            (graphemes::nth_prev_grapheme_boundary(text, pos, count), pos)
                         }
                     }
                 }
@@ -3553,13 +3542,9 @@ pub mod insert {
         let (view, doc) = current!(cx.editor);
         let text = doc.text().slice(..);
         let transaction =
-            Transaction::change_by_selection(doc.text(), doc.selection(view.id), |range| {
+            Transaction::delete_by_selection(doc.text(), doc.selection(view.id), |range| {
                 let pos = range.cursor(text);
-                (
-                    pos,
-                    graphemes::nth_next_grapheme_boundary(text, pos, count),
-                    None,
-                )
+                (pos, graphemes::nth_next_grapheme_boundary(text, pos, count))
             });
         doc.apply(&transaction, view.id);
 
